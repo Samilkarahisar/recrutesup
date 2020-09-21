30ae93a65fcf66d84776d377add3595ae724e2ad
Samil <samilkarahisar@gmail.com>
2020-09-21 15:11:26 +0200

    Initial add back module

---
+++ recrutesup-back/src/test/java/com/polytech/recrutesup/RecrutesupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.recrutesup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutesupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
