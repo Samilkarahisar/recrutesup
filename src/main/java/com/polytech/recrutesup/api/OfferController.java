5d2e25ed840b2d08fe6114b553041b807f9a35a4
remidacosta <remidc2109@gmail.com>
2020-11-18 13:10:24 +0100

    feat: modif détails mineurs

--- src/main/java/com/polytech/recrutesup/api/OfferController.java
+++ src/main/java/com/polytech/recrutesup/api/OfferController.java
@@ -1,9 +1,9 @@
 package com.polytech.recrutesup.api;
 
-import com.polytech.recrutesup.dto.OfferDTO;
-import com.polytech.recrutesup.dto.OfferLightDTO;
-import com.polytech.recrutesup.payload.request.CreateOfferRequest;
-import com.polytech.recrutesup.services.impl.OfferServiceImpl;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,52 +16,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.util.List;
+import com.polytech.recrutesup.dto.OfferDTO;
+import com.polytech.recrutesup.dto.OfferLightDTO;
+import com.polytech.recrutesup.payload.request.CreateOfferRequest;
+import com.polytech.recrutesup.services.dto.OfferServiceDTO;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("offer")
 public class OfferController {
 
-    @Autowired
-    private OfferServiceImpl offerService;
+	@Autowired
+	private OfferServiceDTO offerService;
 
+	@GetMapping("/{idOffer}")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
+	public ResponseEntity<OfferDTO> getOffer(@PathVariable Long idOffer) {
+		return new ResponseEntity<>(this.offerService.getOffer(idOffer), HttpStatus.OK);
+	}
 
-    @GetMapping("/{idOffer}")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
-    public ResponseEntity<OfferDTO> getOffer(@PathVariable Long idOffer) {
-        return new ResponseEntity<>(this.offerService.getOffer(idOffer), HttpStatus.OK);
-    }
+	@GetMapping("/all")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
+	public ResponseEntity<List<OfferDTO>> getAllOffer() {
+		return new ResponseEntity<>(this.offerService.getAllOffer(), HttpStatus.OK);
+	}
 
-    @GetMapping("/all")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
-    public ResponseEntity<List<OfferDTO>> getAllOffer() {
-        return new ResponseEntity<>(this.offerService.getAllOffer(), HttpStatus.OK);
-    }
+	@GetMapping("/allByCompany/{idCompany}")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY')")
+	public ResponseEntity<List<OfferDTO>> getAllOfferByCompany(@PathVariable Long idCompany) {
+		return new ResponseEntity<>(this.offerService.getAllOfferByCompanyId(idCompany), HttpStatus.OK);
+	}
 
-    @GetMapping("/allByCompany/{idCompany}")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY')")
-    public ResponseEntity<List<OfferDTO>> getAllOfferByCompany(@PathVariable Long idCompany) {
-        return new ResponseEntity<>(this.offerService.getAllOfferByCompanyId(idCompany), HttpStatus.OK);
-    }
+	@GetMapping("/list")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
+	public ResponseEntity<List<OfferLightDTO>> getAllOfferLight() {
+		return new ResponseEntity<>(this.offerService.getAllOfferLight(), HttpStatus.OK);
+	}
 
-    @GetMapping("/list")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
-    public ResponseEntity<List<OfferLightDTO>> getAllOfferLight() {
-        return new ResponseEntity<>(this.offerService.getAllOfferLight(), HttpStatus.OK);
-    }
+	@GetMapping("/allByCompany/{idCompany}/list")
+	@PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
+	public ResponseEntity<List<OfferLightDTO>> getAllOfferByCompanyLight(@PathVariable Long idCompany) {
+		return new ResponseEntity<>(this.offerService.getAllOfferByCompanyIdLight(idCompany), HttpStatus.OK);
+	}
 
-    @GetMapping("/allByCompany/{idCompany}/list")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('COMPANY') or hasRole('STUDENT')")
-    public ResponseEntity<List<OfferLightDTO>> getAllOfferByCompanyLight(@PathVariable Long idCompany) {
-        return new ResponseEntity<>(this.offerService.getAllOfferByCompanyIdLight(idCompany), HttpStatus.OK);
-    }
-
-    @PostMapping("/add")
-    @PreAuthorize("hasRole('COMPANY')")
-    public ResponseEntity<OfferDTO> createOffer(@Valid @RequestBody CreateOfferRequest createOfferRequest) {
-        return new ResponseEntity<>(this.offerService.createOffer(createOfferRequest), HttpStatus.OK);
-    }
+	@PostMapping("/add")
+	@PreAuthorize("hasRole('COMPANY')")
+	public ResponseEntity<OfferDTO> createOffer(@Valid @RequestBody CreateOfferRequest createOfferRequest) {
+		return new ResponseEntity<>(this.offerService.createOffer(createOfferRequest), HttpStatus.CREATED);
+	}
 }
--- src/main/java/com/polytech/recrutesup/payload/request/CreateOfferRequest.java
+++ src/main/java/com/polytech/recrutesup/payload/request/CreateOfferRequest.java
@@ -1,37 +1,38 @@
 package com.polytech.recrutesup.payload.request;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateOfferRequest {
 
-    @NotNull
-    @Size(max = 40)
-    private String label;
+	@NotNull
+	@Size(max = 40)
+	private String label;
 
-    @NotNull
-    @Size(max = 500)
-    private String description;
+	@Size(max = 500)
+	private String description;
 
-    @NotNull
-    @Size(max = 40)
-    private String address;
+	@NotNull
+	@Size(max = 40)
+	private String address;
 
-    @NotNull
-    @Size(max = 40)
-    private String city;
+	@NotNull
+	@Size(max = 40)
+	private String city;
 
-    @NotNull
-    @Size(max = 40)
-    private String mailAddress;
+	@NotNull
+	@Size(max = 40)
+	private String mailAddress;
 
-    private List<String> attachmentNamesList;
+	private List<String> attachmentNamesList;
 
-    private Long userId;
+	@NotNull
+	private Long userId;
 }
