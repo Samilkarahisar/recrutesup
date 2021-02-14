856c4b4ae52ba6a20ddd45c1a1db7ab00e6af54d
remidacosta <remidc2109@gmail.com>
2021-02-14 20:37:56 +0100

    feat: modif des properties de l'appli pour externaliser l'envoi des mails auto

--- src/main/java/com/polytech/recrutesup/mail/service/MailService.java
+++ src/main/java/com/polytech/recrutesup/mail/service/MailService.java
@@ -8,6 +8,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,9 @@ public class MailService {
 	
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+	private Environment env;
 
 	private void sendEmail(String destinataire, String objet, String contenu) {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -36,7 +40,13 @@ public class MailService {
             mimeMessage.setContent(contenu, "text/html");
             mimeMessageHelper.setSubject(SUBJECT_PREFIXE + objet);
             mimeMessageHelper.setFrom(new InternetAddress("ptut.recrutesup@gmail.com", "recruteSUP"));
-            mimeMessageHelper.setTo(destinataire);
+            
+            if(env.getProperty("spring.mail.fakeReceiver").equals("true")) {
+            	mimeMessageHelper.setTo("ptut.recrutesup@gmail.com");
+            } else {
+            	mimeMessageHelper.setTo(destinataire);
+            }
+            
  
             mailSender.send(mimeMessageHelper.getMimeMessage());
  
@@ -48,35 +58,35 @@ public class MailService {
 	}
 	
 	public void sendEmailCreationStudentProfil(User user, String mdp) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Création profil", MailObjectGenerator.confirmCreationStudentProfil(user.getFirstname(), user.getLastname(), mdp));
+		this.sendEmail(user.getMailAddress(), "Création profil", MailObjectGenerator.confirmCreationStudentProfil(user.getFirstname(), user.getLastname(), mdp));
 	}
 	
 	public void sendEmailCreationEmployeeProfil(User user, String mdp) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Création profil", MailObjectGenerator.confirmCreationEmployeeProfil(user.getFirstname(), user.getLastname(), mdp));
+		this.sendEmail(user.getMailAddress(), "Création profil", MailObjectGenerator.confirmCreationEmployeeProfil(user.getFirstname(), user.getLastname(), mdp));
 	}
 	
 	public void sendEmailCreationCompanyProfil(Company company) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Création profil", MailObjectGenerator.confirmCreationCompanyProfil(company.getName()));
+		this.sendEmail(company.getMailAddress(), "Création profil", MailObjectGenerator.confirmCreationCompanyProfil(company.getName()));
 	}
 	
 	public void sendEmailRecuperationPassword(String email, String mdp) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Récupération mot de passe", MailObjectGenerator.recuperationPassword(mdp));
+		this.sendEmail(email, "Récupération mot de passe", MailObjectGenerator.recuperationPassword(mdp));
 	}
 	
 	public void sendEmailConfirmationChangePassword(User user) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Modification mot de passe", MailObjectGenerator.changePassword(user.getFirstname(), user.getLastname()));
+		this.sendEmail(user.getMailAddress(), "Modification mot de passe", MailObjectGenerator.changePassword(user.getFirstname(), user.getLastname()));
 	}
 	
 	public void sendEmailCreationCompanyWish(Company company, Student student) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Vous avez reçu un voeu !", MailObjectGenerator.creationCompanyWish(company.getName(), student.getUser().getFirstname()));
+		this.sendEmail(student.getUser().getMailAddress(), "Vous avez reçu un voeu !", MailObjectGenerator.creationCompanyWish(company.getName(), student.getUser().getFirstname()));
 	}
 	
 	public void sendEmailConfirmationStudentWish(Student student, Offer offer) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Vous avez reçu un voeu !", MailObjectGenerator.creationStudentWish(student.getUser().getFirstname(), student.getUser().getLastname(), offer.getCompany().getName(), offer.getLabel()));
+		this.sendEmail(offer.getCreatedByUser().getMailAddress(), "Vous avez reçu un voeu !", MailObjectGenerator.creationStudentWish(student.getUser().getFirstname(), student.getUser().getLastname(), offer.getCompany().getName(), offer.getLabel()));
 	}
 	
 	public void sendEmailMeetingCreationRequestWithDate(String dateMeeting, String message, User sender, Company company, Student interlocutor, User receiver) {
-			this.sendEmail("ptut.recrutesup@gmail.com", "Demande de rencontre",
+			this.sendEmail(receiver.getMailAddress(), "Demande de rencontre",
 					MailObjectGenerator.creationMeetingRequestWithDate(dateMeeting,
 							                                   message,
 							                                   sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
@@ -85,7 +95,7 @@ public class MailService {
 	}
 	
 	public void sendEmailMeetingCreationRequestWithoutDate(String message, User sender, Company company, Student interlocutor, User receiver) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Demande de rencontre",
+		this.sendEmail(receiver.getMailAddress(), "Demande de rencontre",
 				MailObjectGenerator.creationMeetingRequestWithoutDate(message,
 						                                   sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
 						                                   sender.getMailAddress(),
@@ -93,26 +103,30 @@ public class MailService {
 	}
 	
 	public void sendEmailMeetingCreationRequestToAdminWithDate(String dateMeeting, String message, User sender, Company company, Student interlocutor, List<Admin> receivers) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Demande de rencontre",
-				MailObjectGenerator.creationMeetingRequestToAdminWithDate(dateMeeting,
-							                                      message,
-							                                      sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
-							                                      sender.getMailAddress(),
-							                                      interlocutor.getUser().getFirstname() + " " + interlocutor.getUser().getLastname(),
-							                                      interlocutor.getUser().getMailAddress()));
+		for(Admin admin: receivers) {
+			this.sendEmail(admin.getUser().getMailAddress(), "Demande de rencontre",
+					MailObjectGenerator.creationMeetingRequestToAdminWithDate(dateMeeting,
+								                                      message,
+								                                      sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
+								                                      sender.getMailAddress(),
+								                                      interlocutor.getUser().getFirstname() + " " + interlocutor.getUser().getLastname(),
+								                                      interlocutor.getUser().getMailAddress()));
+		}
 	}
 	
 	public void sendEmailMeetingCreationRequestToAdminWithoutDate(String message, User sender, Company company, Student interlocutor, List<Admin> receivers) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Demande de rencontre",
-				MailObjectGenerator.creationMeetingRequestToAdminWithoutDate(message,
-							                                      sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
-							                                      sender.getMailAddress(),
-							                                      interlocutor.getUser().getFirstname() + " " + interlocutor.getUser().getLastname(),
-							                                      interlocutor.getUser().getMailAddress()));
+		for(Admin admin: receivers) {
+			this.sendEmail(admin.getUser().getMailAddress(), "Demande de rencontre",
+					MailObjectGenerator.creationMeetingRequestToAdminWithoutDate(message,
+								                                      sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
+								                                      sender.getMailAddress(),
+								                                      interlocutor.getUser().getFirstname() + " " + interlocutor.getUser().getLastname(),
+								                                      interlocutor.getUser().getMailAddress()));
+		}
 	}
 	
 	public void sendEmailMessage(String message, User sender, Offer offer, User receiver) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Vous avez un message",
+		this.sendEmail(receiver.getMailAddress(), "Vous avez un message",
 				MailObjectGenerator.emailMessage(message,
 		                                         sender.getFirstname() + " " + sender.getLastname(),
 		                                         sender.getMailAddress(),
@@ -121,7 +135,7 @@ public class MailService {
 	}
 	
 	public void sendEmailMessage(String message, User sender, Company company, User receiver) {
-		this.sendEmail("ptut.recrutesup@gmail.com", "Vous avez un message",
+		this.sendEmail(receiver.getMailAddress(), "Vous avez un message",
 				MailObjectGenerator.emailMessage(message,
 		                                         sender.getFirstname() + " " + sender.getLastname() + " (" + company.getName() + ")",
 		                                         sender.getMailAddress(),
